0ecbeec8ca7c964a68de55f5ec4d124d43517095
Ville Kuusela <vkuusela@student.hive.fi>
2025-05-29 20:03:40 +0300

    sphere hit detection simplified

--- src/rendering/hit_detection.c
+++ src/rendering/hit_detection.c
@@ -15,19 +15,19 @@
 float	sphere_intersection(t_sphere sphere, t_ray ray)
 {
 	t_vec3	displacement_vec;
-	float	square_r;
+	float	square_ray;
 	float	projection;
 	float	square_dist;
 	float	discriminant;
 
 	displacement_vec = subtract_vec(sphere.center, ray.origin);
-	square_r = dot_product(ray.direction, ray.direction);
-	projection = -2.0 * dot_product(ray.direction, displacement_vec);
+	square_ray = dot_product(ray.direction, ray.direction);
+	projection = dot_product(ray.direction, displacement_vec);
 	square_dist = dot_product(displacement_vec, displacement_vec)
 		- sphere.radius * sphere.radius;
-	discriminant = projection * projection - 4 * square_r * square_dist;
+	discriminant = projection * projection - square_ray * square_dist;
 	if (discriminant < 0)
 		return (-1.0);
 	else
-		return ((-projection - sqrt(discriminant) / (2.0 * square_r)));
+		return (projection - sqrt(discriminant) / square_ray);
 }
