3600e03ec0c804a0dfd6a60440f6680363fe9a87
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-06-20 15:37:26 +0300

    fix reflect at

--- src/rendering/shading.c
+++ src/rendering/shading.c
@@ -7,13 +7,10 @@ t_vec3	normal_at(t_sphere sphere, t_vec3 point)
 	return (normalize(subtract_vec(point, sphere.center)));
 }
 
-t_vec3	reflect_at(t_ray ray, t_vec3 light_n)
+t_vec3	reflect_at(t_vec3 in_v, t_vec3 light_n)
 {
-	t_vec3	in_v;
 	float	dot;
 
-	in_v = normalize(scale_vec(ray.direction, -1));
-	;
 	dot = dot_product(in_v, light_n);
 	return (subtract_vec(in_v, scale_vec(light_n, 2 * dot)));
 }
@@ -41,9 +38,11 @@ t_rgbcolor	lightining(t_vec3 point, t_objects *objects, t_sphere obj,
 	t_rgbcolor	specular;
 	t_vec3		lightv;
 	float		epsilon;
+	float		reflect_dot_eye;
+	float		factor;
 
-	(void)specular;
-	effective_color = multiply_color_by(normalize_color(obj.color),	objects->l[0].b_ratio);
+	effective_color = multiply_color_by(normalize_color(obj.color),
+			objects->l[0].b_ratio);
 	lightv = normalize(subtract_vec(objects->l[0].coordinates, point));
 	ambient = multiply_color_by(normalize_color(obj.color), objects->a.ratio);
 	epsilon = dot_product(lightv, phong.normal_v);
@@ -55,9 +54,19 @@ t_rgbcolor	lightining(t_vec3 point, t_objects *objects, t_sphere obj,
 	else
 	{
 		diffuse = multiply_color_by(effective_color, epsilon);
+		reflect_dot_eye = dot_product(phong.reflect_V, phong.eye_v);
+		if (reflect_dot_eye > 0)
+		{
+			factor = powf(reflect_dot_eye, 32.0f);
+			specular = multiply_color_by(objects->l[0].color, factor
+					* objects->l[0].b_ratio);
+		}
+		else
+		{
+			specular = (t_rgbcolor){0, 0, 0};
+		}
 	}
-	printf("diffuse is %f , %f, %f \n", diffuse.r, diffuse.g, diffuse.b);
-	return (add_colors(ambient, diffuse));
+	return (add_colors(add_colors(ambient, diffuse), specular));
 }
 
 t_rgbcolor	shading_vectors(t_objects *obj, t_ray ray, float hit, int index)
@@ -74,7 +83,7 @@ t_rgbcolor	shading_vectors(t_objects *obj, t_ray ray, float hit, int index)
 	phong.eye_v = subtract_vec(obj->c.coordinates, hit_point);
 	phong.light_v = subtract_vec(obj->l[0].coordinates, hit_point);
 	phong.normal_v = normal_at(obj->sp[index], hit_point);
-	phong.reflect_V = reflect_at(ray, phong.light_v);
+	phong.reflect_V = reflect_at(normalize(scale_vec(phong.light_v, -1)), phong.normal_v);
 	lightining(hit_point, obj, obj->sp[0], phong);
 	return (lightining(hit_point, obj, obj->sp[0], phong));
 	// final_color = (ambient + diffuse + specular) * sphere_color;
