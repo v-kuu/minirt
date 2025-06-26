ef414921218769e837a2eca7d469f530ce6ee507
Ville Kuusela <vkuusela@student.hive.fi>
2025-06-26 13:31:53 +0300

    cylinders now shade properly, but there are still weird extra shadows

--- src/rendering/cylinder_detection.c
+++ src/rendering/cylinder_detection.c
@@ -13,7 +13,7 @@
 #include "../../minirt.h"
 
 static t_hit	finite_hit(float t, t_ray ray, t_cylinder cyl);
-static t_hit	check_caps(float t1, float t2, t_cylinder cyl, t_ray ray);
+static t_hit	solve_hit(float t1, float t2, t_cylinder cyl, t_ray ray);
 static t_hit	circle_intersection(t_cylinder cyl, t_ray ray, int dir);
 
 t_hit	cylinder_intersection(t_cylinder cyl, t_ray ray)
@@ -34,13 +34,13 @@ t_hit	cylinder_intersection(t_cylinder cyl, t_ray ray)
 		- pow(cyl.diameter, 2);
 	discriminant = projection * projection - squares[0] * squares[1];
 	if (discriminant < 0)
-		return ((t_hit){.t = -1.0f});
-	return (check_caps(((projection - sqrtf(discriminant)) / squares[0]),
+		return ((t_hit){.t = FLT_MAX});
+	return (solve_hit(((projection - sqrtf(discriminant)) / squares[0]),
 			(projection + sqrtf(discriminant) / squares[0]),
 			cyl, ray));
 }
 
-static t_hit	check_caps(float t1, float t2, t_cylinder cyl, t_ray ray)
+static t_hit	solve_hit(float t1, float t2, t_cylinder cyl, t_ray ray)
 {
 	t_hit	t_all[4];
 	t_hit	closest;
@@ -51,15 +51,10 @@ static t_hit	check_caps(float t1, float t2, t_cylinder cyl, t_ray ray)
 	t_all[1] = finite_hit(t2, ray, cyl);
 	t_all[2] = circle_intersection(cyl, ray, 1);
 	t_all[3] = circle_intersection(cyl, ray, -1);
-	index = 0;
-	while (index < 4)
-	{
-		if (t_all[index].t >= 0 && t_all[index].t < closest.t)
+	index = -1;
+	while (++index < 4)
+		if (t_all[index].t < FLT_MAX && t_all[index].t < closest.t)
 			closest = t_all[index];
-		index++;
-	}
-	if (closest.t == FLT_MAX)
-		closest.t = -1.0;
 	closest.color = shading_visual(cyl.color);
 	return (closest);
 }
@@ -76,16 +71,10 @@ static t_hit	circle_intersection(t_cylinder cyl, t_ray ray, int dir)
 			(t_vec3){0, dir, 0});
 	denominator = dot_product(ray.direction, (t_vec3){0, 1 * dir, 0});
 	if (fabs(denominator) < 1e-6)
-	{
-		ret.t = -1.0;
-		return (ret);
-	}
+		return ((t_hit){.t = FLT_MAX});
 	ret.t = temp / denominator;
 	if (vec_len(subtract_vec(ray_at(ray, ret.t), cap_point)) > cyl.diameter)
-	{
-		ret.t = -1.0;
-		return (ret);
-	}
+		return ((t_hit){.t = FLT_MAX});
 	ret.normal = rotate_by_quat(inverse_quat(cyl.q_axis), (t_vec3){0, dir, 0});
 	return (ret);
 }
@@ -98,7 +87,7 @@ static t_hit	finite_hit(float t, t_ray ray, t_cylinder cyl)
 	hit = ray_at(ray, t);
 	if (hit.y > cyl.height / 2 || hit.y < cyl.height / -2)
 	{
-		ret.t = -1;
+		ret.t = FLT_MAX;
 		return (ret);
 	}
 	ret.t = t;
